b538a0f98419dddfbbc7d8da710239ff3ee06431
Josue Emanuel Hernandez Ortega <josue30759@gmail.com>
2022-11-20 17:50:15 -0600

    pantalones a

--- html/hombres/categorias/camisa-hombre.php
+++ html/hombres/categorias/camisa-hombre.php
@@ -21,11 +21,11 @@ Page::headerTemplate($url1, $url2, $link1, $link2); // clase de php
 
                 <div class="apartado-filtro">
 
-                    <div class="boton"><button id="vestido">Sacos</button></div>
-                    <div class="boton"><button id="camisa">Camisas</button></div>
-                    <div class="boton"><button id="abrigo">Abrigos</button></div>
-                    <div class="boton"><button id="chaqueta">Formal</button></div>
-                    <div class="boton"><button id="deportivo">Deportivas</button></div>
+                    <div class="boton"><button id="Sacos">Sacos</button></div>
+                    <div class="boton"><button id="Camisas">Camisas</button></div>
+                    <div class="boton"><button id="Abrigos">Abrigos</button></div>
+                    <div class="boton"><button id="Formal">Formal</button></div>
+                    <div class="boton"><button id="Deportivas">Deportivas</button></div>
 
                 </div>
 
