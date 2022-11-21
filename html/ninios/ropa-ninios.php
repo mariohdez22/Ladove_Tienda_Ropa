9f7416c39ea482d68d29a2d2d54f2913f9734af5
ProChicken2001 <dennis4000.dapu@gmail.com>
2022-11-21 13:57:40 -0600

    ninios

--- html/ninios/ropa-ninios.php
+++ html/ninios/ropa-ninios.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../../template/menu-footer.php'); //importamos el footer
 $url1 = '../../css/menu-footer.css'; //importamos los estilos css del footer
-$url2 = '../../css/ninios/ropa-ninios.css'; //importamos los estilos css para el documento html
+$url2 = '../../css/ninios/ropaNinios.css'; //importamos los estilos css para el documento html
 $link1 = '../index.php';
 $link2 = '../index.php';
 Page::headerTemplate($url1,$url2,$link1,$link2);
