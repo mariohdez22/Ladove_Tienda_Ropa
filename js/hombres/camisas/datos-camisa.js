550e51eb2ccf51eef37804cca84b292ae594ee8a
Monvall <105135454+Monvall@users.noreply.github.com>
2022-11-23 01:48:10 -0600

    hombres lobby

--- js/hombres/camisas/datos-camisa.js
+++ js/hombres/camisas/datos-camisa.js
@@ -1,24 +1,24 @@
 export const datosN = 
 [
-    {id: 1, enunciado: 'tipoB', img: '../../../multimedia/saco1.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (70.40).toFixed(2)},
-    {id: 1, enunciado: 'F blanca', img: '../../../multimedia/camisah1.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (90.00).toFixed(2)},
-    {id: 1, enunciado: 'abrigo T saco', img: '../../../multimedia/abrigoh1.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (50.20).toFixed(2)},
-    {id: 1, enunciado: 'blanca ml', img: '../../../multimedia/vestir1.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (80.00).toFixed(2)},
+    {id: 1, enunciado: 'tipoB', img: '../../../multimedia/saco1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (70.40).toFixed(2)},
+    {id: 1, enunciado: 'F blanca', img: '../../../multimedia/camisah1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (90.00).toFixed(2)},
+    {id: 1, enunciado: 'abrigo T saco', img: '../../../multimedia/abrigoh1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (50.20).toFixed(2)},
+    {id: 1, enunciado: 'blanca ml', img: '../../../multimedia/vestir1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (80.00).toFixed(2)},
 
-    {id: 2, enunciado: 'clasicoC', img: '../../../multimedia/saco2.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (30.50).toFixed(2)},
-    {id: 2, enunciado: 'deportiva s', img: '../../../multimedia/deportivah1.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (40.00).toFixed(2)},
-    {id: 2, enunciado: 'chaqueta', img: '../../../multimedia/saco5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (70.40).toFixed(2)},
-    {id: 2, enunciado: 'campo s', img: '../../../multimedia/camisah5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (90.00).toFixed(2)},
+    {id: 2, enunciado: 'clasicoC', img: '../../../multimedia/saco2.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (30.50).toFixed(2)},
+    {id: 2, enunciado: 'deportiva s', img: '../../../multimedia/deportivah1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (40.00).toFixed(2)},
+    {id: 2, enunciado: 'chaqueta', img: '../../../multimedia/saco5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (70.40).toFixed(2)},
+    {id: 2, enunciado: 'campo s', img: '../../../multimedia/camisah5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (90.00).toFixed(2)},
 
-    {id: 3, enunciado: 'abrigo carme', img: '../../../multimedia/abrigoh5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (30.50).toFixed(2)},
-    {id: 3, enunciado: 'clasica a', img: '../../../multimedia/vestir5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (40.00).toFixed(2)},
-    {id: 3, enunciado: 'bmw t', img: '../../../multimedia/deportivo5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (70.40).toFixed(2)},
-    {id: 3, enunciado: 'abrigo carmi', img: '../../../multimedia/saco8.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (90.00).toFixed(2)},
+    {id: 3, enunciado: 'abrigo carme', img: '../../../multimedia/abrigoh5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (30.50).toFixed(2)},
+    {id: 3, enunciado: 'clasica a', img: '../../../multimedia/vestir5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (40.00).toFixed(2)},
+    {id: 3, enunciado: 'bmw t', img: '../../../multimedia/deportivo5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (70.40).toFixed(2)},
+    {id: 3, enunciado: 'abrigo carmi', img: '../../../multimedia/saco8.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (90.00).toFixed(2)},
 
-    {id: 4, enunciado: 'carmesy', img: '../../../multimedia/camisah8.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (30.50).toFixed(2)},
-    {id: 4, enunciado: 'abrigo moto', img: '../../../multimedia/abrigoh4.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (40.00).toFixed(2)},
-    {id: 4, enunciado: 'abrigo cT', img: '../../../multimedia/abrigoh8.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (65.50).toFixed(2)},
-    {id: 4, enunciado: 'naranja mn', img: '../../../multimedia/vestir4.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (45.00).toFixed(2)},
+    {id: 4, enunciado: 'carmesy', img: '../../../multimedia/camisah8.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (30.50).toFixed(2)},
+    {id: 4, enunciado: 'abrigo moto', img: '../../../multimedia/abrigoh4.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (40.00).toFixed(2)},
+    {id: 4, enunciado: 'abrigo cT', img: '../../../multimedia/abrigoh8.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (65.50).toFixed(2)},
+    {id: 4, enunciado: 'naranja mn', img: '../../../multimedia/vestir4.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (45.00).toFixed(2)},
 ]
 
 export const datos1 = 
@@ -26,7 +26,7 @@ export const datos1 =
     {id: 1, enunciado: 'tipoB', img: '../../../multimedia/saco1.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (70.50).toFixed(2)},
     {id: 1, enunciado: 'clasicoC', img: '../../../multimedia/saco2.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (75.00).toFixed(2)},
     {id: 1, enunciado: 'formalF', img: '../../../multimedia/saco3.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (80.40).toFixed(2)},
-    {id: 1, enunciado: 'clasicoG', img: '../../../multimedia/saco4.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (90.00).toFixed(2)},
+    {id: 1, enunciado: 'clasicoG', img: '../../../multimedia/saco4.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (90.00).toFixed(2)},
 
     {id: 2, enunciado: 'chaqueta', img: '../../../multimedia/saco5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (65.50).toFixed(2)},
     {id: 2, enunciado: 'clasicoA', img: '../../../multimedia/saco6.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (85.00).toFixed(2)},
@@ -41,10 +41,10 @@ export const datos2 =
     {id: 1, enunciado: 'clasica', img: '../../../multimedia/camisah3.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (40.00).toFixed(2)},
     {id: 1, enunciado: 'T chaqueta b', img: '../../../multimedia/camisah4.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (45.50).toFixed(2)},
 
-    {id: 2, enunciado: 'campo s', img: '../../../multimedia/camisah5.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (55.00).toFixed(2)},
-    {id: 2, enunciado: 'M larga', img: '../../../multimedia/camisah6.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (48.00).toFixed(2)},
-    {id: 2, enunciado: 'M larga b', img: '../../../multimedia/camisah7.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (35.00).toFixed(2)},
-    {id: 2, enunciado: 'M larga c', img: '../../../multimedia/camisah8.jpg', link:'../../../html/mujeres/ropa-hombre.php', precio: (37.00).toFixed(2)},
+    {id: 2, enunciado: 'campo s', img: '../../../multimedia/camisah5.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (55.00).toFixed(2)},
+    {id: 2, enunciado: 'M larga', img: '../../../multimedia/camisah6.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (48.00).toFixed(2)},
+    {id: 2, enunciado: 'M larga b', img: '../../../multimedia/camisah7.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (35.00).toFixed(2)},
+    {id: 2, enunciado: 'M larga c', img: '../../../multimedia/camisah8.jpg', link:'../../../html/hombres/ropa-hombre.php', precio: (37.00).toFixed(2)},
 ]
 
 export const datos3 =
